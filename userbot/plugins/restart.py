0b411294b3479d9bf33144fefcdea9fc25e7d6e8
LeaderFighter <87716278+LeaderFighter@users.noreply.github.com>
2022-06-05 11:29:18 +0530

    Add files via upload

---
+++ userbot/plugins/restart.py
@@ -0,0 +1,50 @@
+import sys
+from time import sleep
+
+from userbot import catub
+
+from ..core.logger import logging
+from ..core.managers import edit_or_reply
+from ..sql_helper.global_collection import (
+    add_to_collectionlist,
+    del_keyword_collectionlist,
+    get_collectionlist_items,
+)
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from . import BOTLOG, BOTLOG_CHATID, HEROKU_APP
+
+LOGS = logging.getLogger(__name__)
+plugin_category = "tools"
+
+
+@catub.cat_cmd(
+    pattern="restart$",
+    command=("restart", plugin_category),
+    info={
+        "header": "Restarts the bot !!",
+        "usage": "{tr}restart",
+    },
+)
+async def _(event):
+    "Restarts the bot !!"
+    if BOTLOG:
+        await event.client.send_message(BOTLOG_CHATID, "#RESTART \n" "Bot Restarted")
+    sandy = await edit_or_reply(
+        event,
+        "Restarted. `.ping` me or `.help` to check if I am online, actually it takes 1-2 min for restarting",
+    )
+    try:
+        ulist = get_collectionlist_items()
+        for i in ulist:
+            if i == "restart_update":
+                del_keyword_collectionlist("restart_update")
+    except Exception as e:
+        LOGS.error(e)
+    try:
+        add_to_collectionlist("restart_update", [sandy.chat_id, sandy.id])
+    except Exception as e:
+        LOGS.error(e)
+    try:
+        await catub.disconnect()
+    except Exception as e:
+        LOGS.error(e)
